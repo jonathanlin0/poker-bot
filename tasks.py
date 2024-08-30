8b44c202ed95a3a5e5080570a1a568786b3a76bf
Jonathan Lin <jonathanlinonline@gmail.com>
2024-08-30 16:25:30 -0500

    ruff for formatting

--- tasks.py
+++ tasks.py
@@ -4,3 +4,14 @@ from invoke import task
 def type_check(c):
     """Run mypy for static type checking."""
     c.run("mypy .")
+
+@task
+def format(c):
+    """Run ruff for code formatting."""
+    c.run("RUFF_CACHE_DIR=cache/.ruff_cache ruff check . --fix")
+
+@task
+def format_check(c):
+    """Check for formatting issues using ruff."""
+    c.run("RUFF_CACHE_DIR=cache/.ruff_cache ruff check .")
+    
